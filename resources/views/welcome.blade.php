718bfacb89e4ef7384eac97337de0a5e3db8364e
Avdeev Pavel <avdeevpavel19@gmail.com>
2022-06-30 13:24:41 +0300

    output info if product was not found during search

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,24 +9,32 @@
         <div class="container">
             <div class="main">
                 <div class="products">
-                    @foreach($products as $product)
-                        <div class="product">
-                            <img class="product-image" src="{{asset('storage/' . $product->image)}}" alt="">
-                            <div class="product-text">
-                                <h3 class="product-price">{{$product->price}}</h3>
-                                <a class="product-title" href="{{route('catalog.show', $product->id)}}">{{$product->title}}</a>
+                    @if(count($products))
+                        @foreach($products as $product)
+                            <div class="product">
+                                <img class="product-image" src="{{asset('storage/' . $product->image)}}" alt="">
+                                <div class="product-text">
+                                    <h3 class="product-price">{{$product->price}}</h3>
+                                    <a class="product-title"
+                                       href="{{route('catalog.show', $product->id)}}">{{$product->title}}</a>
+                                </div>
+
+                                @if(auth()->user())
+                                    <form class="addCart" action="{{route('add.product.request')}}" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="product_id" value="{{$product->id}}">
+
+                                        <button class="product-btn">В корзину</button>
+                                    </form>
+                                @endif
                             </div>
-
-                            @if(auth()->user())
-                                <form class="addCart" action="{{route('add.product.request')}}" method="POST">
-                                    @csrf
-                                    <input type="hidden" name="product_id" value="{{$product->id}}">
-
-                                    <button class="product-btn">В корзину</button>
-                                </form>
-                            @endif
+                        @endforeach
+                    @else
+                        <div class="productIsMissing">
+                            <h3 class="productIsMissing__h3">Товар отсуствует</h3>
+                            <a href="{{route('catalog.index')}}" class="productIsMissing__a">На главную</a>
                         </div>
-                    @endforeach
+                    @endif
                 </div>
             </div>
         </div>
