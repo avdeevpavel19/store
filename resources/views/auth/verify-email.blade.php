f04fa58f42557e73ed01a40b52e25af39b90c352
Avdeev Pavel <avdeevpavel19@gmail.com>
2022-07-01 12:23:17 +0300

    add styles

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -3,7 +3,25 @@
 @section('title', 'Подтвердите email')
 
 @section('content')
-    <h3 class="wrapper__title">Вам отправлено письмо с подтверждением.Перейдите по ссылке</h3>
-
-    <a href="{{route('verification.send')}}">Отправить повторно</a>
+    <h3 class="title">Вам отправлено письмо с подтверждением.Перейдите по ссылке</h3>
+    <a class="sendRepeat" href="{{route('verification.send')}}">Отправить повторно</a>
 @endsection
+
+<style>
+    .title {
+        padding-top: 80px;
+        margin: 0 auto;
+        width: 1060px;
+        font-size: 32px;
+        font-weight: 500;
+    }
+
+    .sendRepeat {
+        color: #04b;
+        font-size: 16px;
+        padding-top: 10px;
+        margin: 0 auto;
+        display: block;
+        width: 165px;
+    }
+</style>
