0b66506180673dcb8863394c84f38989eaa1ba9b
Avdeev Pavel <avdeevpavel19@gmail.com>
2022-07-12 10:37:51 +0300

    handle exceptions in Catalog/MainController

--- app/Http/Controllers/Catalog/MainController.php
+++ app/Http/Controllers/Catalog/MainController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Cart;
 use App\Models\Product;
 use App\Models\Review;
-use http\Client\Curl\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -19,54 +18,63 @@ class MainController extends Controller
 
     public function showProduct($id)
     {
-        $product = Product::where('id', $id)->first();
+        $product = Product::where('id', $id)->firstOrFail();
 
         $reviews = Review::with('user')
-            ->where('product_id', Product::find($id)->id)
+            ->where('product_id', Product::findOrFail($id)->id)
             ->get();
 
-        if($product) {
+        if ($product) {
             return view('catalog.show', compact('product', 'reviews'));
         }
     }
 
-    public function categoryProduct($id) {
+    public function categoryProduct($id)
+    {
         $categoryProduct = Product::where('category_id', $id)->get();
         return view('catalog.category-product', compact('categoryProduct'));
     }
 
-    public function searchProduct(Request $request) {
+    public function searchProduct(Request $request)
+    {
         $products = Product::where('title', 'LIKE', "%{$request->search}%")->get();
         return view('welcome', compact('products'));
     }
 
-    public function addReview($id) {
-        $product = Product::find($id);
+    public function addReview($id)
+    {
+        $product = Product::findOrFail($id);
         return view('catalog.review-form', compact('product'));
     }
 
-    public function addReviewRequest(Request $request, $id) {
-        $review = Review::create([
-            'user_id' => auth()->user()->id,
-            'product_id' => Product::find($id)->id,
-            'like' => $request->input('like'),
-            'dislike' => $request->input('dislike'),
-            'other_impressions' => $request->input('other_impressions')
-        ]);
+    public function addReviewRequest(Request $request, $id)
+    {
+        try {
+            Review::create([
+                'user_id' => auth()->user()->id,
+                'product_id' => Product::find($id)->id,
+                'like' => $request->input('like'),
+                'dislike' => $request->input('dislike'),
+                'other_impressions' => $request->input('other_impressions')
+            ]);
 
-        if ($review) {
             return redirect()->route('catalog.show', $id);
+        } catch (\Illuminate\Database\QueryException $queryException) {
+            return abort(500);
         }
     }
 
-    public function addProductRequest(Request $request) {
-        $addProduct = Cart::create([
-           'user_id' => auth()->user()->id,
-           'product_id' => $request->input('product_id')
-        ]);
+    public function addProductRequest(Request $request)
+    {
+        try {
+            Cart::create([
+                'user_id' => auth()->user()->id,
+                'product_id' => $request->input('product_id')
+            ]);
 
-        if($addProduct) {
             return redirect()->route('catalog.index');
+        } catch (\Illuminate\Database\QueryException $queryException) {
+            return abort(500);
         }
     }
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,41 @@
+<head>
+    <title>404</title>
+</head>
+<body class="centered">
+<div class="emoji"><img src="{{asset('images/404.gif')}}" width=250 alt=""></div>
+
+<p class="title">Упс!</p>
+<p class="text">Кажется, страница не найдена<br></p>
+</body>
+
+<style>
+    html {
+        font-size: 62.5%;
+    }
+    body {
+        background-color: #fff;
+        color: #000;
+        font-family: helvetica, arial, sans-serif;
+        font-size: 1.4em;
+        line-height: 1.5;
+    }
+    .centered {
+        position: fixed;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    .emoji {
+        font-size: 9em;
+        text-align: center;
+    }
+    .title {
+        font-size: 3em;
+        text-align: center;
+        line-height: 0em;
+        color: grey;
+    }
+    .text {
+        text-align: center;
+    }
+</style>
